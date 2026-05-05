a92cc0752b820edf2434bc1be1033a14f63f424c
VictorNicolau-coder <victoranderson1575@gmail.com>
2026-05-05 18:07:26 -0300

    second commit

--- src/scripts/Q1.sql
+++ src/scripts/Q1.sql
@@ -1,8 +1,11 @@
 --- Panorama temporal e ranking
 USE chocolatesales;
 
-CREATE VIEW vw_ranking (store, sales_quantity) AS
-SELECT s.store_name, q.quantity
-FROM stores s
-INNER JOIN sales q
-ON q.store_id = s.store_id;
+CREATE OR REPLACE VIEW vw_ranking AS
+SELECT stores.store_name, SUM(sales.quantity) AS totalQuantity, stores.country
+FROM sales 
+INNER JOIN stores ON sales.store_id = stores.store_id
+WHERE order_date BETWEEN '2024-12-01' AND '2024-12-31'
+    AND country = 'Germany'
+GROUP BY stores.store_name
+ORDER BY totalQuantity DESC;
---
+++ src/scripts/Q2.sql
@@ -0,0 +1,11 @@
+--- Monitorar qualidade de dados e priorização
+USE chocolatesales;
+
+CREATE OR REPLACE VIEW vw_ranking AS
+SELECT stores.store_name, SUM(sales.quantity) AS totalQuantity, stores.country
+FROM sales 
+INNER JOIN stores ON sales.store_id = stores.store_id
+WHERE order_date BETWEEN '2024-12-01' AND '2024-12-31'
+    AND country = 'Germany'
+GROUP BY stores.store_name
+ORDER BY totalQuantity DESC;
